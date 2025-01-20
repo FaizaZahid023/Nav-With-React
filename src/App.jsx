35d3cca0f9f884071c33771bf5a8d603d3be7851
User name <faizazahidali123@gmail.com>
2025-01-20 03:15:33 -0800

    navebar

---
+++ src/App.jsx
@@ -0,0 +1,62 @@
+
+import { useState } from 'react'
+import './App.css'
+import { Routes, Route, Link, Navigate } from "react-router";
+import About from './pages/About'
+import Home from './pages/Home'
+import Contact from './pages/Contact'
+import Product from './pages/Product'
+import Signup from './pages/Signup'
+import Login from './pages/Login'
+
+
+function App() {
+  const [isLogin, setIsLogin] = useState(true);
+   return (
+    <>
+      <nav className="navbar">
+        <div className="logo">Navbar</div>
+     
+        {
+          (isLogin) ?
+
+            <div className='link'  >
+              <Link to="/">Home</Link>
+              <Link to="/about">About</Link>
+              <Link to="/profile">Profile</Link>
+              <Link to="/contact">Contact</Link>
+            </div>
+            :
+
+
+            <div className='link' >
+              <Link to="/">Login</Link>
+              <Link to="/signup">Signup</Link>
+            </div>
+
+        }
+        <button id="Toggle" onClick={() => { setIsLogin(!isLogin) }}>SignUp button</button>
+      </nav>
+      {
+        (isLogin) ?
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/about' element={<About />} />
+            <Route path='/contact' element={<Contact />} />
+            <Route path='/product' element={<Product />} />
+            <Route path="/signup" element={<Navigate to="/" />} />
+          </Routes> :
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="*" element={<Navigate to="/" />} />
+
+          </Routes>
+
+
+      }
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/Product.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+const Product = () => {
+  return (
+    <h1>Product page</h1>
+  )
+}
+
+export default Product
